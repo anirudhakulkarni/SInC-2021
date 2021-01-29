fb9b287048087e27eb8a830250061d8479f8d88f
Himanshu Anuragi <himanshu122122@gmail.com>
2021-01-30 01:21:22 +0530

    filter page improved and fixed bootstrap and tailwind issue

--- assets/js/filterlist.js
+++ assets/js/filterlist.js
@@ -13,6 +13,9 @@ var data = [
     var thumbLeft = document.querySelector(".slider > .thumb.left");
     var thumbRight = document.querySelector(".slider > .thumb.right");
     var range = document.querySelector(".slider > .range");
+    
+   
+    setFromYears();
 
    showStartupsInTable(data); //shows the whole data on load of page
 
@@ -23,11 +26,32 @@ var data = [
        showStartupsInTable(Newdata);
    });
 
+   $(`#expand`).on(`click`, function(){ // looks for changes in search and filters the data accordingly
+    var filters = document.getElementById("filters");
+    });
+
    function onlyOne(checkbox) { //handels the checkbox of team strength filter
         var checkboxes = document.getElementsByName('team')
         checkboxes.forEach((item) => {
             if (item !== checkbox) item.checked = false
         })
+    }
+
+    function setFromYears(){
+        var fromYear = document.getElementById("fromYear");
+        fromYear.innerHTML = '';
+        var toYear = document.getElementById("toYear");
+        toYear.innerHTML = '';
+    
+       for(var i = 2000; i <= 2021; i++){
+           var row = `			      
+                        <div @click="show=!show" class="cursor-pointer text-black border-gray-100 rounded-t border-b hover:bg-teal-100">
+                            ${i}
+                        </div>
+                     `
+           fromYear.innerHTML += row
+           toYear.innerHTML += row
+       }
     }
 
     
@@ -112,6 +136,8 @@ var data = [
     showStartupsInTable(data);
    }
 
+    
+
    function showStartupsInTable(data){ //main function that adds tags/cards to main area based on the array(data) passed to it
    var table = document.getElementById("startups");
    table.innerHTML = ''
@@ -127,46 +153,26 @@ var data = [
        }
 
        var row = `			      
-                <div class="card col-6 mb-2 mx-auto" >
-                <div class="row no-gutters">
-                    <div class="col-4">
-                    <img src="https://unsplash.it/300/300?image=503" class="card-img" alt="">
-                    </div>
-                    <div class="col-8">
-                    <div class="card-body">
-                        <div class="flex justify-content-between"> 
-                        <h5>StartUp Name</h5>
-                        <p class="card-text"> <span class="${spanClass}">${data[i].stage}</span></p>
-                        
-
-                      
-                        </div>
-
-                        <p>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                        </p>
-                        
-                       
-                        <div class="collapse" id="${data[i].startUp}">
-                        
-                        <p>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh
-                        helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-                        </p>
-                        
-                        </div>
-                        <button
-                            class="py-1 px-6 bg-red-600 text-white focus:outline-none"
-                            data-toggle="collapse" data-target="#${data[i].startUp}"
-                        >
-                            View More
-                        </button>
-                        
-                    
-                    </div>
-                    </div>
-                </div>
-                </div>
+       <figure class="md:w-3/4 lg:w-1/2 md:flex bg-gray-100 rounded-xl p-8 p-0 mx-auto">
+       <img class="sm:w-30 sm:h-30 w-40 h-40 md:w-70 md:h-auto rounded-none rounded-full mx-auto" src="https://picsum.photos/200" alt="" width="384" height="512">
+       <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+         <blockquote>
+           <p class="text-lg font-semibold">
+             “Tailwind CSS is the only framework that I've seen scale
+             on large teams. It’s easy to customize, adapts to any design,
+             and the build size is tiny.”
+           </p>
+         </blockquote>
+         <figcaption class="font-medium">
+           <div class="text-cyan-600">
+             Sarah Dayan
+           </div>
+           <div class="text-gray-500">
+             Staff Engineer, Algolia
+           </div>
+         </figcaption>
+       </div>
+     </figure>
                  `
        table.innerHTML += row
    }
